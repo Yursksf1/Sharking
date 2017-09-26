5e9defdd8f4e7e9950072b52077c147addadc052
Yurley Sánchez <yurs.ksf1@gmail.com>
2017-09-25 22:25:07 -0500

    load css and statics

---
+++ static/vuejs/djangovue.js
@@ -0,0 +1,35 @@
+
+
+Vue.config.delimiters = ["[[","]]"]
+
+var demo = new Vue({
+    el: '#app',
+    data: {
+        'apptitle': 'Django Vue.JS Job Board',
+        'jobs': []
+    },
+    methods: {
+        addJob: function () {
+            var newJob = {
+                jobtitle: this.jobtitle.trim(),
+                jobdescription: this.jobdescription.trim()
+            };
+
+            this.$http.post('http://127.0.0.1:8000/joblist/api/joblist/', newJob);
+        },
+        removeJob: function (index) {
+            this.$http.delete('http://127.0.0.1:8000/joblist/api/joblist/'.concat(this.jobs[index].id));
+            this.jobs.splice(index, 1);
+        }
+    },
+    ready: function()
+        {
+            this.$http.get('http://127.0.0.1:8000/joblist/api/joblist/').then(function (response) {
+                this.jobs = response.data;
+            },
+            function (response) {
+                console.log(response);
+            });
+        }
+});
+
